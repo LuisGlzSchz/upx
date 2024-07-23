4ddd0fb2c9bf7605831c3055855eccc4589b07c7
Luis González <luisf.gonzalezs@hotmail.com>
2024-07-23 12:30:50 -0600

    DOM JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
-document.getElementById('main-btn').addEventListener('click', function() {
-    var extraButtons = document.getElementById('extra-buttons');
-    if (extraButtons.classList.contains('d-none')) {
-      extraButtons.classList.remove('d-none');
-      extraButtons.style.display = 'flex';
-    } else {
-      extraButtons.classList.add('d-none');
-      extraButtons.style.display = 'none';
-    }
+// document.getElementById('main-btn').addEventListener('click', function() {
+//     var extraButtons = document.getElementById('extra-buttons');
+//     if (extraButtons.classList.contains('d-none')) {
+//       extraButtons.classList.remove('d-none');
+//       extraButtons.style.display = 'flex';
+//     } else {
+//       extraButtons.classList.add('d-none');
+//       extraButtons.style.display = 'none';
+//     }
+//   });
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('main-btn').addEventListener('click', function() {
+      var extraButtons = document.getElementById('extra-buttons');
+      if (extraButtons.classList.contains('d-none')) {
+          extraButtons.classList.remove('d-none');
+          extraButtons.style.display = 'flex';
+      } else {
+          extraButtons.classList.add('d-none');
+          extraButtons.style.display = 'none';
+      }
   });
+});
